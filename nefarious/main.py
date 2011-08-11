94916027317c44d5a8e3b60b4144ba6cf6278600
Timothee Groleau <tg@timotheegroleau.com>
2011-08-11 23:09:15 +0800

    saner cli interface, prompts when overwriting input file

--- nefarious/main.py
+++ nefarious/main.py
@@ -2,52 +2,74 @@ import optparse
 import copy
 import nefarious
 
+
+def confirmSave(tiff, input, output, force=False):
+
+    if output != input:
+        tiff.save(output)
+        return 0
+
+    else:
+        msg = "This will overwrite your input nef. Are you sure? (y/n) "
+        if force:
+            print "%s%s" % (msg, 'y')
+            overwrite = True
+        else:
+            overwrite = raw_input(msg).strip().lower() == 'y'
+
+        if overwrite:
+            tiff.save(input)
+            return 0
+        else:
+            return 1
+
+
+
 def main():   
     parser = optparse.OptionParser()
     
-    parser.add_option("-f","--input-file",dest="input")
-    parser.add_option("-t","--thumbnail", dest="thumb")
-    parser.add_option("-i","--info",dest="info", action="store_true")
-    parser.usage = "nef-cli -f filename.nef -t thumb.jpg filename.tiff"
+    parser.add_option("-i", "--input-file",   dest="input",   help="Specify the input nef file")
+    parser.add_option("-o", "--output-file",  dest="output",  help="Specify the file to write to")
+    parser.add_option("-p", "--preview-jepg", dest="preview", help="Specify the jpeg file to use as nef preview")
+    parser.add_option("-y", "--yes",          dest="yes",     help="Automatically answer yes when prompted to overwrite input", action="store_true", default=False)
+
+    parser.usage = "nef-cli -i in.nef [-p preview.jpg] [-o out.nef]"
     opts, args = parser.parse_args()
     
-    tiff = nefarious.core.TiffImage()
     
     if not opts.input:
-        parser.error("please specify input file with the -f option")
-    if opts.input and (not opts.thumb or not len(args)==1) and not opts.info:
-        parser.error("please specify thumbnail image (-t) and output filename")
-   
+        parser.error("please specify input file with the -i option")
+
+    tiff = nefarious.core.TiffImage()
     tiff.load(opts.input)
-    if opts.info:
-        print tiff.toString()
-    else:
+
+    if opts.preview:
+        # make a backup of the original preview image as a new ifd in the same nef
+        # need some smarter logic here and options here...
         # if len(tiff.frames[0].ifds) < 3:
         #    tiff.frames[0].ifds.append( copy.deepcopy(tiff.frames[0].ifds[0]) )
             
-        f = open(opts.thumb)
+        f = open(opts.preview)
         jpeg = f.read()
         f.close()
             
         tiff.frames[0].ifds[0].imageType = "JPEG"
         tiff.frames[0].ifds[0].imageData = jpeg
         tiff.frames[0].ifds[0].tags_by_code[nefarious.constants.TAG_JPEG_INTERCHANGE_FORMAT_LENGTH].data[0] = len(jpeg)
-            
-        tiff.save(opts.input)
-    
-    
-    #tiff.save(sys.argv[1] + ".tiff")
-    
-    #tiff2 = TiffImage()
-    #tiff2.load(sys.argv[1] + ".tiff")
-    #print tiff2.toString()
-    
-    
-    # assume this is a nef file...
-    # fp = open("%s.thumb.jpg" % sys.argv[1], 'w')
-    # fp.write( tiff.frames[0].ifds[0].imageData )
-    # fp.close()
-    
-    # now injects jpeg and process data into ifd#0subifd#0
+
+        if opts.output:
+            return confirmSave(tiff, opts.input, opts.output, opts.yes)
+        else:
+            return confirmSave(tiff, opts.input, opts.input, opts.yes)
+
+
+    elif opts.output:
+        return confirmSave(tiff, opts.input, opts.output, opts.yes)
+        
+    else:
+        print tiff.toString()
+        return 0
+
+
 if __name__ == "__main__":
     main()
